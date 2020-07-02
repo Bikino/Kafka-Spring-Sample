3ca37cdae01becec9bc2fd715ef26b92d520bcd7
Ildephonse BIKINO <ildephonsebikino@Ildephonses-MacBook-Pro.local>
2020-07-01 22:20:45 -0700

    Kafka with Spring sample codes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Kafka-Spring-Sample
+
+- INSTALL KAFKA ON MAC - https://www.tutorialkart.com/apache-kafka/install-apache-kafka-on-mac/
+
+
+- RUN ZOOKEEPER (in Terminal) 
+    - sh bin/zookeeper-server-start.sh config/zookeeper.properties
+- START KAFKA (in Terminal) 
+    - sh bin/kafka-server-start.sh config/server.properties	
+- CREATE TOPIC (in Terminal) 
+    - bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic Kafka_Example
+- CREATE PRODUCER (in Terminal)
+    - bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic Kafka_Example --from-beginning
+    
+    
+    
+    Source : https://www.youtube.com/watch?v=NjHYWEV_E_o  
---
+++ src/main/java/com/bkn/kafka/springbootkafkaproducerexample/config/KafkaConfiguration.java
@@ -0,0 +1,38 @@
+package com.bkn.kafka.springbootkafkaproducerexample.config;
+
+
+import com.bkn.kafka.springbootkafkaproducerexample.model.User;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfiguration {
+
+    @Bean
+    public ProducerFactory producerFactory(){
+
+        Map<String,Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, User> kafkaTemplate(){
+        return new KafkaTemplate<String,User>(producerFactory());
+    }
+}
